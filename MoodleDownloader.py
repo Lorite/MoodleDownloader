b9626ec9f3b9bcef51eb77928bbc3e6fe12a9152
Alejandro Lorite <alejandrolorite@opendeusto.es>
2018-09-09 19:57:10 +0200

    Testing commit

--- MoodleDownloader.py
+++ MoodleDownloader.py
@@ -1,1 +1,2 @@
-print("Program made by Alejandro Lorite studying @ University of Deusto for his and everyone's convenience. :)");
+print("Program made by Alejandro Lorite studying @ University of Deusto for his and everyone's convenience. :)")
+print("Downloading completed!")
